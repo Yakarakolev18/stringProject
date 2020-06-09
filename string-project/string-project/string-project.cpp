ab5a7e137697e4fce27306420e764a364820502c
kvbolashikova18 <59692632+kvbolashikova18@users.noreply.github.com>
2020-06-09 20:10:07 +0300

    create main menu

--- string-project/string-project/string-project.cpp
+++ string-project/string-project/string-project.cpp
@@ -57,7 +57,63 @@ bool generatingWords() {
 
 }
 
-int main() {
+void textMenu() {
+	int option;
+	string fullText;
+	getline(cin, fullText);
+	cout << endl;
 
+	cout << "What do you want to know about this text? " << endl;
+	cout << endl;
+
+	cout << "1. How many words in total it consists of"<<endl;
+	cout << "2. How many sentences in total it contains" << endl;
+	cout << "3. If a specific word is present in the text"<<endl;
+	cout << "4. How many times a specific word is mentioned"<<endl;
+	cout << "5. Which are the most commonly used words" << endl;
+	cout << endl;
+
+	cout << "Enter an option: ";
+	cin >> option;
+}
+
+void gamesMenu() {
+	
+	cout << "1. Hangman" << endl;
+	cout << "2. Riddles" << endl;
+}
+
+void mainMenu(){
+	cout << "\t\t\t    HELLO!" << endl;
+	cout << "--------------------------------------------------------------" << endl;
+	cout << "Welcome to our newly developed program connected with strings.\nBelow, as you can see, is located our main menu from which you \ncan select and try out one or more of our program's features. \nWe hope you like it!" << endl;
+	cout << "--------------------------------------------------------------" << endl;
+	cout << "\t\t       --------------" << endl;
+	cout << "\t\t ---  |   MAIN MENU  |  ---" << endl;
+	cout << "\t\t       --------------" << endl;
+	cout << endl;
+
+	int option;
+
+	cout << "1. Input a text and get analysis about it" << endl;
+	cout << "2. Play games" << endl;
+	cout << "3. Exit" << endl;
+
+	cout << endl;
+	cout << "Enter an option: ";
+	cin >> option;
+	cout << endl;
+
+	switch (option) {
+	case 1:textMenu(); break;
+	case 2:gamesMenu(); break;
+	case 3:exit(0); break;
+	}
+}
+
+int main() {
 	srand(time(0));
+
+	mainMenu();
+	system("PAUSE");
 }
