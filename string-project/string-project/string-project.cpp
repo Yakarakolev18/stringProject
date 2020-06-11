b154803c00af2c6a8a691b569ca788c85c79a252
kvbolashikova18 <59692632+kvbolashikova18@users.noreply.github.com>
2020-06-11 15:22:01 +0300

    Add functions "isWordInTextMenu" and "isWordInText" and edit function "wordCountMenu"

--- string-project/string-project/string-project.cpp
+++ string-project/string-project/string-project.cpp
@@ -6,60 +6,50 @@
 using namespace std;
 
 struct WORD {
-	string word="";
-	int count=0;
+	string word = "";
+	int count = 0;
 };
 
-//returns random integer in a range
 long int randomInt(int min, int max) {
 
 	int r = rand();
-	long int randNum = r % ( max - min ) + min;
-
+	long int randNum = r % (max - min) + min;
 	return randNum;
 }
 
-//generates a random word from a file by using the random number
 string generatingWords() {
 
 	int randomNumber;
-	string generatedWord;
 
-	//vector to store the words in
+	string line;
+	string myText;
+
 	vector <string> lines;
 	int totalLines = 0;
-	string line;
+	string generatedWord;
 
-	//opens the file
 	ifstream file("words.txt");
 	if (!file.is_open()) {
 
-		//if the file can't be opened
 		return "Error opening file!";
 	}
 
-	//counts all the lines in the file
 	while (getline(file, line)) {
-		
+
 		totalLines++;
 		lines.push_back(line);
 	}
 
-	//generates the random number
 	randomNumber = randomInt(0, lines.size());
 
-	//puts the chosen word in a variable
 	generatedWord = lines[randomNumber];
 
-	//closes the file
 	file.close();
 
-	//returns the generated word
 	return generatedWord;
 
 }
 
-//counts the left attempts and used letters and shows them
 void processWord(string wantedWord, int numberOfLetters, char inputSymbol, string& wantedWordHidden, vector<char>& inputLetters) {
 
 	for (int i = 1; i <= numberOfLetters - 2; i++) {
@@ -70,35 +60,29 @@ void processWord(string wantedWord, int numberOfLetters, char inputSymbol, strin
 		}
 	}
 
-	//vector which contains the used letters
 	inputLetters.push_back(inputSymbol);
 	cout << "\nYour entered letters: ";
 
-	//shows the used letters
 	for (int i = 0; i < inputLetters.size(); i++) {
 
 		cout << inputLetters[i] << " ";
 	}
 	cout << endl;
 
-	//calculates the attempts needed for a word to be guessed
 	int maximumNumberOfAttempts = numberOfLetters + 3 - inputLetters.size();
 
-	//shows the number of left attempts
 	if (maximumNumberOfAttempts > 0) {
 
-		//if there are some available
 		cout << "You have " << maximumNumberOfAttempts << " attempts left\n" << endl;
 		cout << wantedWordHidden << endl;
 
-	} else {
+	}
+	else {
 
-		//if there are none available
 		cout << "You have 0 attempts left" << endl;
 	}
 }
 
-//exchanges letters to dashes to hide the word
 void processHiddenWord(string wantedWord, string& wantedWordHidden) {
 
 	int numberOfLetters = wantedWord.length();
@@ -107,20 +91,15 @@ void processHiddenWord(string wantedWord, string& wantedWordHidden) {
 
 		wantedWordHidden[i] = '-';
 	}
-
-	//shows max number of attempts
 	cout << "\nYou are entitled to " << numberOfLetters + 3 << " attempts\n" << endl;
 	cout << wantedWordHidden << endl;
 }
 
-//hangman game
 bool gameHangman() {
 
 	string wantedWord, wantedWordHidden;
 	vector<char> inputLetters;
 	int again;
-
-	//gets the word
 	wantedWord = generatingWords();
 
 	wantedWordHidden = wantedWord;
@@ -128,34 +107,30 @@ bool gameHangman() {
 	int maximumNumberOfAttempts = numberOfLetters + 3;
 	char inputSymbol;
 
-	//hides the letters
 	processHiddenWord(wantedWord, wantedWordHidden);
 
 	while (wantedWord.compare(wantedWordHidden) != 0 && maximumNumberOfAttempts > 0) {
 
-		cout << "\nInput letter: ";
+		cout << "\nImput letter: ";
 		cin >> inputSymbol;
 		maximumNumberOfAttempts--;
 		processWord(wantedWord, numberOfLetters, inputSymbol, wantedWordHidden, inputLetters);
 		cout << endl;
 	}
 
-	//results
 	if (maximumNumberOfAttempts > 0) {
 
-		//if the word is guessed
 		cout << endl;
 		cout << "You win!" << endl;
 		inputLetters.clear();
-	} else {
+	}
+	else {
 
-		//if the word is not guessed
 		cout << endl;
-		cout << "You lost! The word was " << wantedWord <<"." << endl;
+		cout << "You lost! The word was " << wantedWord << "." << endl;
 		inputLetters.clear();
 	}
 
-	//asks if another round is wanted
 	cout << endl;
 	cout << "Would you like to play again?" << endl;
 	cout << "1. Yes, let's go!" << endl;
@@ -168,12 +143,10 @@ bool gameHangman() {
 	switch (again) {
 
 	case 1:
-		//if it is wanted
 		return true;
 		break;
 
 	case 2:
-		//if it isn't wanted
 		return false;
 		break;
 
@@ -184,7 +157,59 @@ bool gameHangman() {
 	}
 
 	return true;
+
 }
+
+
+
+
+
+int addWordsInArray(WORD* wordsInText, string fullText) {
+
+	char symbols[] = { '.','!','?',',' };
+	int wordIndex = 0;
+	int firstLetter = 0;
+
+	for (int i = 0; i < fullText.size(); i++) {
+		if (fullText[i] == ' ') {
+
+			int wordLength = i - firstLetter;
+			wordsInText[wordIndex].word = fullText.substr(firstLetter, wordLength);
+			wordIndex++;
+			firstLetter = i + 1;
+		}
+	}
+
+	if (fullText[fullText.size() - 1] != ' ') {
+
+		wordsInText[wordIndex].word = fullText.substr(firstLetter);
+		wordIndex++;
+	}
+
+	for (int i = 0; i < wordIndex; i++) {
+
+		for (int j = 0; j < 4; j++) {
+
+			if (wordsInText[i].word[wordsInText[i].word.size() - 1] == symbols[j]) {
+
+				wordsInText[i].word.erase(wordsInText[i].word.size() - 1);
+			}
+		}
+	}
+
+
+	for (int i = 0; i < wordIndex; i++) {
+
+		for (int j = 0; j < wordsInText[i].word.size(); j++) {
+
+			wordsInText[i].word[j] = tolower(wordsInText[i].word[j]);
+		}
+
+	}
+
+	return wordIndex;
+}
+
 
 int counting(string fullText, char symbols[], int size) {
 
@@ -217,26 +242,46 @@ int counting(string fullText, char symbols[], int size) {
 	return count;
 }
 
-void wordCountMenu(string fullText) {
-
-	char symbols[] = {' '};
-	int wordCount = counting(fullText, symbols, 1);
-
-	cout << "The total amount of words in this text is: " << wordCount + 1 << endl;
+void wordCountMenu(int wordCount) {
+	cout << "The total amount of words in this text is: " << wordCount << endl;
 	cout << endl;
 }
 
 void sentenceCountMenu(string fullText) {
 
-	char symbols[] = { '.','!','?'};
+	char symbols[] = { '.','!','?' };
 	int sentenceCount = counting(fullText, symbols, 3);
 
 	cout << "The total amount of sentences in this text is: " << sentenceCount << endl;
 	cout << endl;
 }
 
-void isWordInTextMenu() {
+bool isWordInText(WORD *wordsInText, int wordCount, string wordForCheck) {
+	int check = 0;
 
+	for (int i = 0; i < wordCount; i++)
+	{
+		if (wordsInText[i].word == wordForCheck)
+			check++;
+	}
+	
+	if (check>0)
+		return true;
+	else
+		return false;
+}
+
+void isWordInTextMenu(WORD *wordsInText, int wordCount) {
+	string wordForCheck;
+	cout << "Enter the word you want to search for: ";
+	cin >> wordForCheck;
+	cout << endl;
+
+	if (isWordInText(wordsInText, wordCount, wordForCheck) == true)
+		cout << "Yes, \"" << wordForCheck << "\" is present in the text!" << endl;
+	else
+		cout << "No, \"" << wordForCheck << "\" is not present in the text!" << endl;
+	cout << endl;
 }
 
 void howManyTimesIsWordInTextMenu() {
@@ -247,49 +292,7 @@ void mostCommonWordsMenu() {
 
 }
 
-void addWordsInArray(WORD *wordsInText, string fullText) {
 
-	char symbols[] = { '.','!','?',',' };
-	int wordIndex = 0;
-	int firstLetter = 0;
-
-	for (int i = 0; i < fullText.size(); i++) {
-		if (fullText[i] == ' ') {
-
-			int wordLength = i - firstLetter;
-			wordsInText[wordIndex].word = fullText.substr(firstLetter, wordLength);
-			wordIndex++;
-			firstLetter = i + 1;
-		}
-	}
-
-	if (fullText[fullText.size() - 1] != ' ') {
-
-		wordsInText[wordIndex].word = fullText.substr(firstLetter);
-	    wordIndex++;
-	}
-
-	for (int i = 0; i < wordIndex; i++){
-
-		for (int j = 0; j < 4; j++) {
-
-			if (wordsInText[i].word[wordsInText[i].word.size() - 1] == symbols[j]) {
-
-				wordsInText[i].word.erase(wordsInText[i].word.size() - 1);
-			}
-		}
-	}
-
-
-	for (int i = 0; i < wordIndex; i++) {
-
-		for (int j = 0; j < wordsInText[i].word.size(); j++) {
-
-			wordsInText[i].word[j] = tolower(wordsInText[i].word[j]);
-		}
-
-	}
-}
 
 
 void textMenu() {
@@ -298,14 +301,16 @@ void textMenu() {
 	string fullText;
 	WORD wordsInText[100];
 
-	cout << "(To quit entering text, press \"Enter\")"<<endl;
+	cout << "(To quit entering text, press \"Enter\")" << endl;
 	cout << endl;
 
 	cin.ignore();
-	getline(cin, fullText,'\n');
+	getline(cin, fullText, '\n');
 	cout << endl;
 
-	addWordsInArray(wordsInText,fullText);
+	int wordCount = addWordsInArray(wordsInText, fullText);
+
+
 
 	bool continueMenu = 0;
 
@@ -319,28 +324,30 @@ void textMenu() {
 		cout << "5. Most commonly used words" << endl;
 		cout << "6. Return back to the main menu" << endl;
 		cout << endl;
-	
 
-	cout << "Your choice: ";
-	cin >> option;
-	cout << endl;
 
-	switch (option) {
+		cout << "Your choice: ";
+		cin >> option;
+		cout << endl;
 
-	case 1:
-		wordCountMenu(fullText);
-		break;
+		switch (option) {
 
-	case 2:
-		sentenceCountMenu(fullText);
-		break;
-		/*case 3:isWordInTextMenu(); break;
-		case 4:howManyTimesIsWordInTextMenu(); break;
-		case 5:mostCommonWordsMenu(); break;*/
-	case 6:
-		continueMenu = 1;
-		break;
-	};
+		case 1:
+			wordCountMenu(wordCount);
+			break;
+
+		case 2:
+			sentenceCountMenu(fullText);
+			break;
+		case 3:
+			isWordInTextMenu(wordsInText,wordCount);
+			break;
+			/*case 4:howManyTimesIsWordInTextMenu(); break;
+			case 5:mostCommonWordsMenu(); break;*/
+		case 6:
+			continueMenu = 1;
+			break;
+		};
 
 	};
 }
@@ -378,7 +385,7 @@ void gamesMenu() {
 	};
 }
 
-bool mainMenu(){
+bool mainMenu() {
 
 	int option;
 	string dashes, lessDashes;
@@ -403,24 +410,24 @@ bool mainMenu(){
 
 	switch (option) {
 
-		case 1:
-			textMenu();
-			break;
+	case 1:
+		textMenu();
+		break;
 
-		case 2:
-			gamesMenu(); 
-			break;
+	case 2:
+		gamesMenu();
+		break;
 
-		case 3:
-			cout << "Thanks for using our program. See you soon!" << endl;
-			return false;
-			break;
+	case 3:
+		cout << "Thanks for using our program. See you soon!" << endl;
+		return false;
+		break;
 
-		default:
-			cout << "There seems to be a problem with your input. "
-				"Please try again." << endl;
-			cout << endl;
-			break;
+	default:
+		cout << "There seems to be a problem with your input. "
+			"Please try again." << endl;
+		cout << endl;
+		break;
 	}
 
 	return true;
@@ -453,7 +460,7 @@ int main() {
 	greetingsMessage();
 
 	do {
-		
+
 		showMainMenu = mainMenu();
 
 	} while (showMainMenu);
